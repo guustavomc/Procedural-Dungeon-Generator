413612524d217658dcb364f747e277c25460e47e
guustavomc <gustavomccb@gmail.com>
2026-06-19 11:39:16 -0300

    implemented room and corridor classes

--- dungeon-generator/room.py
+++ dungeon-generator/room.py
@@ -18,3 +18,18 @@ class Rect:
     @property 
     def center(self):
         return (self.x_rect_top_left_corner+ self.rect_width // 2), (self.y_rect_top_left_corner+ self.rect_height // 2)
+    
+@dataclass
+class Room:
+    rect: Rect
+    id: int
+    
+    @property
+    def center(self):
+        return self.rect.center
+    
+@dataclass
+class Corridor:
+    center_room_A: tuple[int, int]
+    center_room_B: tuple[int, int]
+    center_L_shaped_corner: tuple[int, int]
