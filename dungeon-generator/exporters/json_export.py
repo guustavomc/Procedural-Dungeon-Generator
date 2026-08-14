3f6f48128857b2021ae3dee4aa62633388482d86
guustavomc <gustavomccb@gmail.com>
2026-08-14 08:57:17 -0300

    feat: include room and corridor attributes in JSON export for enhanced data representation

--- dungeon-generator/exporters/json_export.py
+++ dungeon-generator/exporters/json_export.py
@@ -9,7 +9,10 @@ def export(dungeon):
                 "x": r.rect.x_rect_top_left_corner,
                 "y": r.rect.y_rect_top_left_corner,
                 "width": r.rect.rect_width,
-                "height": r.rect.rect_height
+                "height": r.rect.rect_height,
+                "id": r.id,
+                "type": r.room_type.name,
+                "center": list(r.center)
             }
             for r in dungeon.rooms
         ],
@@ -17,7 +20,9 @@ def export(dungeon):
             {
                 "start": list(c.center_room_A),
                 "end": list(c.center_room_B),
-                "bend": list(c.center_L_shaped_corner)
+                "bend": list(c.center_L_shaped_corner),
+                "room_a_id": c.room_a_id,
+                "room_b_id": c.room_b_id
             }
             for c in dungeon.corridors
         ],
