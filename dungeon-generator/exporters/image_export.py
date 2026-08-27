1f981d3d2e724cee17587fdfd21801fe121f87d4
guustavomc <gustavomccb@gmail.com>
2026-08-27 08:25:12 -0300

    feat: implement image export functionality with room type colors

---
+++ dungeon-generator/exporters/image_export.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageDraw
+
+from room_type import RoomType
+
+WALL_COLOR = (30, 30, 30)
+CORRIDOR_COLOR = (170, 140, 100)
+
+ROOM_COLORS = {
+    RoomType.ENTRANCE: (80, 200, 120),
+    RoomType.EXIT: (200, 60, 60),
+    RoomType.TREASURE: (230, 200, 40),
+    RoomType.BOSS: (150, 40, 180),
+    RoomType.NORMAL: (200, 200, 200),
+}
+
+def export(dungeon, tile_size=16):
+    img = Image.new("RGB", (dungeon.width * tile_size, dungeon.height * tile_size), WALL_COLOR,)
+    draw = ImageDraw.Draw(img)
